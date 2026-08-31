a800f15a04b7aede3d827e0a250719bd3b5fbc04
Opticks <allysontuliostd@gmail.com>
2026-08-31 12:58:36 -0300

    feat: cria estrutura inicial do servidor express

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.json({ mensagem: "Servidor do SpeakFlow rodando com sucesso!" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT} (http://localhost:${PORT})`);
+});
+
+
